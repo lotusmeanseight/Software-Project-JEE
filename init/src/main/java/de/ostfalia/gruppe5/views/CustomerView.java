9a7b27cf6c82736b20088550158a08380ad1c73f
rblumenau <rblu@hotmail.de>
2018-10-01 02:49:46 +0200

    update and delete in datatable implemented.

--- init/src/main/java/de/ostfalia/gruppe5/views/CustomerView.java
+++ init/src/main/java/de/ostfalia/gruppe5/views/CustomerView.java
@@ -3,11 +3,18 @@ package de.ostfalia.gruppe5.views;
 import de.ostfalia.gruppe5.models.Customer;
 import de.ostfalia.gruppe5.services.CustomerService;
 
+import javax.annotation.ManagedBean;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
+@ManagedBean
 @RequestScoped
 @Named
 public class CustomerView {
@@ -45,8 +52,28 @@ public class CustomerView {
         detachedCustomer.setLastname(this.customer.getLastname());
         service.merge(detachedCustomer);
     }
+    public void update(String id) {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
 
-    public void delete() {
+        Customer detachedCustomer = service.findById(Long.valueOf(id));
+        service.detachCustomer(detachedCustomer);
+        Date birthdayDate= null;
+        try {
+            birthdayDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(request.getParameter("customer_birthdate_input_"+id));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        detachedCustomer.setBirthdate(birthdayDate);
+        detachedCustomer.setFirstname(request.getParameter("customer_firstname_input_"+id));
+        detachedCustomer.setLastname(request.getParameter("customer_lastname_input_"+id));
+        service.merge(detachedCustomer);
+    }
+
+    public void delete(String id) {
+        service.deleteById(Long.valueOf(id));
+	}
+
+	public void delete() {
         service.deleteById(customer.getId());
 	}
 }
