7d325012d0674d4f777f8b05f05b2ecf755a110a
rblumenau <rblu@hotmail.de>
2018-10-09 23:26:05 +0200

    updating facesContext

--- init/src/main/java/de/ostfalia/gruppe5/services/CustomerService.java
+++ init/src/main/java/de/ostfalia/gruppe5/services/CustomerService.java
@@ -3,9 +3,11 @@ package de.ostfalia.gruppe5.services;
 import de.ostfalia.gruppe5.models.Customer;
 
 import javax.ejb.Stateless;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Map;
 
 
 @Stateless
@@ -14,8 +16,14 @@ public class CustomerService {
     @PersistenceContext(unitName = "simple")
     EntityManager em;
 
-    public CustomerService() {
+    FacesContext facesContext = FacesContext.getCurrentInstance();
 
+    public CustomerService() {
+        Map<Object, Object> tmp = facesContext.getAttributes();
+        System.out.println("tmp size: " + tmp.size());
+        for (Object o : tmp.keySet()) {
+            System.out.println(o + ": " + tmp.get(o));
+        }
     }
 
     public void save(Customer customer) {
@@ -31,7 +39,7 @@ public class CustomerService {
     }
 
     public List<Customer> getAllCustomers() {
-        return em.createQuery("SELECT c from Customer c",Customer.class).getResultList();
+        return em.createQuery("SELECT c from Customer c", Customer.class).getResultList();
     }
 
 
