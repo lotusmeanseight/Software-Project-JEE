5bbadafb22bdeb180805bca3d3943c7bb7a9dc3e
lotusmeanseight <lotusmeansflower@tutanota.com>
2018-10-24 23:37:11 +0200

    officeservice angepasst

--- sep/src/main/java/de/ostfalia/gruppe5/services/OfficeService.java
+++ sep/src/main/java/de/ostfalia/gruppe5/services/OfficeService.java
@@ -1,13 +1,11 @@
 package de.ostfalia.gruppe5.services;
 
-import java.util.List;
+import de.ostfalia.gruppe5.models.Office;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
-import de.ostfalia.gruppe5.models.Office;
+import java.util.List;
 
 @Stateless
 public class OfficeService {
@@ -15,10 +13,6 @@ public class OfficeService {
 
     @PersistenceContext(name = "simple")
     EntityManager entityManager;
-    
-   //for delete method
-    @Inject 
-	EmployeeService empService;
 
     public OfficeService() {
 
@@ -31,17 +25,6 @@ public class OfficeService {
     private Office findById(String id) {
         return entityManager.find(Office.class, id);
     }
-    
-//  public boolean canBeDeleted(String id){
-//	   List<Employee> emps = empService.getAllEmployees();
-//	   boolean isEmployeInOffice = false;
-//	   for(Employee emp : emps){
-//	     if(id = emp.getOfficeCode()){
-//	        return false;
-//	     }
-//	   return true;
-//	   }
-//	   }
 
     public void deleteById(String id) {
             entityManager.remove(findById(id));
@@ -52,16 +35,7 @@ public class OfficeService {
     }
 
     public Office update(Office office) {
-    	Office detachedOffice = this.findById(office.getOfficeCode());
-		   detachedOffice.setCity(office.getCity());
-		   detachedOffice.setCity(office.getPhone());
-		   detachedOffice.setCity(office.getAddressLine1());
-		   detachedOffice.setCity(office.getAddressLine2());
-		   detachedOffice.setCity(office.getState());
-		   detachedOffice.setCity(office.getCountry());
-		   detachedOffice.setCity(office.getPostalCode());
-		   detachedOffice.setCity(office.getTerritory());
-		   return detachedOffice;
+        return entityManager.merge(office);
     }
 }
 
