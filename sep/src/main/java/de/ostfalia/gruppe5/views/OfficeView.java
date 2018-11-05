c3b2ece1743fe6daa0e0d51e4f44ea2c695d05c8
BastiHu <bastianhuppertz@gmail.com>
2018-11-05 21:55:42 +0100

    Added Pagination for the rest of the views

--- sep/src/main/java/de/ostfalia/gruppe5/views/OfficeView.java
+++ sep/src/main/java/de/ostfalia/gruppe5/views/OfficeView.java
@@ -3,6 +3,7 @@ package de.ostfalia.gruppe5.views;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.component.html.HtmlDataTable;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -17,12 +18,39 @@ public class OfficeView {
 	@Inject
 	private OfficeService service;
 
+	private HtmlDataTable table;
+	private int rowsOnPage = 10;
+
 	public OfficeView() {
 		setOffice(new Office());
 	}
 
 	public List<Office> getOffices() {
 		return service.getAllOffices();
+	}
+
+	public Office getOffice() {
+		return office;
+	}
+
+	public void setOffice(Office office) {
+		this.office = office;
+	}
+
+	public HtmlDataTable getTable() {
+		return table;
+	}
+
+	public void setTable(HtmlDataTable table) {
+		this.table = table;
+	}
+
+	public int getRowsOnPage() {
+		return rowsOnPage;
+	}
+
+	public void setRowsOnPage(int rowsOnPage) {
+		this.rowsOnPage = rowsOnPage;
 	}
 
 	public String save() {
@@ -40,11 +68,30 @@ public class OfficeView {
 		return null;
 	}
 
-	public Office getOffice() {
-		return office;
+	public void goToFirstPage() {
+		table.setFirst(0);
 	}
 
-	public void setOffice(Office office) {
-		this.office = office;
+	public void goToPreviousPage() {
+		table.setFirst(table.getFirst() - table.getRows());
 	}
+
+	public void goToNextPage() {
+		table.setFirst(table.getFirst() + table.getRows());
+	}
+
+	public void goToLastPage() {
+		int totalRows = table.getRowCount();
+		int displayRows = table.getRows();
+		int full = totalRows / displayRows;
+		int modulo = totalRows % displayRows;
+
+		if (modulo > 0) {
+			table.setFirst(full * displayRows);
+		} else {
+			table.setFirst((full - 1) * displayRows);
+		}
+
+	}
+
 }
