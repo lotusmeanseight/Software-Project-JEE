46d7f211e7c856a98fb6a1d9956e76a860ba4e69
lotusmeanseight <lotusmeansflower@tutanota.com>
2018-11-14 00:49:03 +0100

    fixes order table in pagination for employee

--- sep/src/main/java/de/ostfalia/gruppe5/business/boundary/AbstractLazyJPAService.java
+++ sep/src/main/java/de/ostfalia/gruppe5/business/boundary/AbstractLazyJPAService.java
@@ -1,11 +1,15 @@
 package de.ostfalia.gruppe5.business.boundary;
 
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+@DeclareRoles({"EMPLOYEE, CUSTOMER"})
 public abstract class AbstractLazyJPAService<T> extends AbstractJPAService<T> {
 
+    @RolesAllowed("EMPLOYEE")
     public List<T> getAllTLazy(int first, int max){
         TypedQuery<T> query = getEntityManager().createNamedQuery(gettClass().getSimpleName() + ".findAll", gettClass());
         query.setFirstResult(first);
@@ -13,6 +17,7 @@ public abstract class AbstractLazyJPAService<T> extends AbstractJPAService<T> {
         return query.getResultList();
     }
 
+    @RolesAllowed("EMPLOYEE")
     public int countT(){
         Query query = getEntityManager().createNamedQuery(gettClass().getSimpleName() + ".countAll" );
         List<Long> resultList = query.getResultList();
