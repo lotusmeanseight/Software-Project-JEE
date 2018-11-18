43e370589169a9b658efed16e970c732d8c311d9
lotusmeanseight <lotusmeansflower@tutanota.com>
2018-11-18 17:56:36 +0100

    basic productline rest ressource

---
+++ sep/src/main/java/de/ostfalia/gruppe5/business/boundary/ProductLineRessource.java
@@ -0,0 +1,39 @@
+package de.ostfalia.gruppe5.business.boundary;
+
+import de.ostfalia.gruppe5.business.entity.ProductLine;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@RolesAllowed("EMPLOYEE")
+@Stateless
+@Path("productlines")
+public class ProductLineRessource {
+
+    public ProductLineRessource(){
+    }
+
+    @Inject
+    private ProductLineService service;
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<ProductLine> getProductLines(){
+        return service.findAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ProductLine getProductLine(@PathParam("id") String id){
+        return service.find(id);
+    }
+}
