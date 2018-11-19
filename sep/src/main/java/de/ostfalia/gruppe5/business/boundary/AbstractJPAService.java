4def871f196c6f6b481517e7b29625ec3822eb7d
BastiHu <bastianhuppertz@gmail.com>
2018-11-20 00:37:31 +0100

    added POST , PUT , DELETE for Office and Customer

--- sep/src/main/java/de/ostfalia/gruppe5/business/boundary/AbstractJPAService.java
+++ sep/src/main/java/de/ostfalia/gruppe5/business/boundary/AbstractJPAService.java
@@ -1,5 +1,7 @@
 package de.ostfalia.gruppe5.business.boundary;
 
+import java.util.List;
+
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
@@ -8,66 +10,65 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import java.util.List;
 
-@DeclareRoles({"EMPLOYEE, CUSTOMER"})
+@DeclareRoles({ "EMPLOYEE, CUSTOMER" })
 @RolesAllowed("EMPLOYEE")
 public abstract class AbstractJPAService<T> {
 
-    private Class<T> entityClass;
+	private Class<T> entityClass;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    public T find(final String id){
-        return getEntityManager().find(entityClass, id);
-    }
+	public T find(final String id) {
+		return getEntityManager().find(entityClass, id);
+	}
 
-    public T find(final Integer id){
-        return getEntityManager().find(entityClass, id);
-    }
+	public T find(final Integer id) {
+		return getEntityManager().find(entityClass, id);
+	}
 
-    public List<T> findAll(){
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(entityClass);
-        Root<T> root = criteriaQuery.from(entityClass);
-        CriteriaQuery<T> all = criteriaQuery.select(root);
-        TypedQuery<T> allQuery = entityManager.createQuery(all);
+	public List<T> findAll() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(entityClass);
+		Root<T> root = criteriaQuery.from(entityClass);
+		CriteriaQuery<T> all = criteriaQuery.select(root);
+		TypedQuery<T> allQuery = entityManager.createQuery(all);
 
-        return allQuery.getResultList();
-    }
+		return allQuery.getResultList();
+	}
 
-    public void save(final T entity){
-        getEntityManager().persist(entity);
-    }
+	public void save(final T entity) {
+		getEntityManager().persist(entity);
+	}
 
-    public void delete(final T entity){
-        getEntityManager().remove(entity);
-    }
+	public void delete(final T entity) {
+		getEntityManager().remove(entity);
+	}
 
-    public void deleteById(final String id){
-        getEntityManager().remove(find(id));
-    }
+	public void deleteById(final String id) {
+		getEntityManager().remove(find(id));
+	}
 
-    public void deleteById(final Integer id){
-        getEntityManager().remove(find(id));
-    }
+	public void deleteById(final Integer id) {
+		getEntityManager().remove(find(id));
+	}
 
-    public T update(final T entity){
-        return getEntityManager().merge(entity);
-    }
+	public T update(final T entity) {
+		return getEntityManager().merge(entity);
+	}
 
-    public Class<T> getEntityClass() {
-        return entityClass;
-    }
+	public Class<T> getEntityClass() {
+		return entityClass;
+	}
 
-    public void setEntityClass(Class<T> entityClass) {
-        this.entityClass = entityClass;
-    }
+	public void setEntityClass(Class<T> entityClass) {
+		this.entityClass = entityClass;
+	}
 
-    @RolesAllowed({"EMPLOYEE,CUSTOMER"})
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }
+	@RolesAllowed({ "EMPLOYEE,CUSTOMER" })
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
 
 }
--- sep/src/main/java/de/ostfalia/gruppe5/business/boundary/rest/OfficeRessource.java
+++ sep/src/main/java/de/ostfalia/gruppe5/business/boundary/rest/OfficeRessource.java
@@ -1,41 +1,102 @@
 package de.ostfalia.gruppe5.business.boundary.rest;
 
-import de.ostfalia.gruppe5.business.boundary.OfficeService;
-import de.ostfalia.gruppe5.business.entity.Office;
+import java.net.URI;
+import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import de.ostfalia.gruppe5.business.boundary.OfficeService;
+import de.ostfalia.gruppe5.business.entity.Office;
 
 @RolesAllowed("EMPLOYEE")
 @Stateless
 @Path("offices")
 public class OfficeRessource {
 
-    @Inject
-    private OfficeService service;
+	@Inject
+	private OfficeService service;
 
-    public OfficeRessource(){
-    }
+	@Context
+	private UriInfo uriinfo;
 
-    @GET
-    @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Office> getOffices(){
-        return service.findAll();
-    }
+	public OfficeRessource() {
+	}
 
-    @GET
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Office getOffice(@PathParam("id") Integer id){
-        return service.find(id);
-    }
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getOffices() {
+		List<Office> offices = service.findAll();
+		if (offices.isEmpty()) {
+			return Response.status(Status.NOT_FOUND).build();
+		} else {
 
+			return Response.ok(offices).build();
+		}
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Office getOffice(@PathParam("id") Integer id) {
+		return service.find(id);
+	}
+
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createOffice(Office office) {
+
+		service.save(office);
+		Office o = service.find(office.getOfficeCode());
+		UriBuilder builder = uriinfo.getRequestUriBuilder();
+		URI uri = builder.path(OfficeRessource.class, "getOffice").build(o.getOfficeCode());
+		return Response.created(uri).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response deleteOffice(@PathParam("id") Integer id) {
+		Office office = service.find(id);
+		if (office == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		} else {
+			service.delete(office);
+			return Response.ok().build();
+		}
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateOffice(@PathParam("id") Integer id, Office office) {
+		if (id != office.getOfficeCode()) {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+		Office o = service.update(office);
+		if (o == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		} else {
+			return Response.ok().build();
+		}
+
+	}
 }
