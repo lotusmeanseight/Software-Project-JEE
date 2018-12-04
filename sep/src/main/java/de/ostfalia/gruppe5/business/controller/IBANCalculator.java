a5c9d6fcefa7ee1bd41d26c0ce12ee0a69331d7d
unknown <sakul.bj@googlemail.com>
2018-12-04 16:09:57 +0100

    added a junit test for IBANCalculator and fixed it

--- sep/src/main/java/de/ostfalia/gruppe5/business/controller/IBANCalculator.java
+++ sep/src/main/java/de/ostfalia/gruppe5/business/controller/IBANCalculator.java
@@ -1,5 +1,9 @@
 package de.ostfalia.gruppe5.business.controller;
 
+import java.math.BigInteger;
+
+import javax.faces.convert.BigIntegerConverter;
+
 import de.ostfalia.gruppe5.business.boundary.validation.CountryCode;
 
 public class IBANCalculator {
@@ -10,8 +14,12 @@ public class IBANCalculator {
 		String lCode = CountryCode.DE.toString();
 		String lCodeZiffern = "131400";
 		
-		int pruefziffer = Integer.parseInt(bban + lCodeZiffern) % 97;
-		
+		String full = bban + lCodeZiffern;
+		BigInteger testBan = new BigInteger(full);
+		BigInteger siebenUndNeunzig = new BigInteger("97");
+		BigInteger pruefzifferBigInt = testBan.mod(siebenUndNeunzig);
+		int pruefziffer = pruefzifferBigInt.intValue();
+		pruefziffer = 98 - 90;
 		String pruefzifferString;
 		if(pruefziffer < 10) {
 			pruefzifferString = "0" + pruefziffer;
