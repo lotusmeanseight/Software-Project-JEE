5b119e20f22e218f9d5e9138c1fa760daecd99d1
rblumenau <rblu@hotmail.de>
2018-12-05 22:14:33 +0100

    added tests for basket rest endpoint in OrderCustomerIT and OrderEmployeeIT

--- sep/src/test/java/de/ostfalia/gruppe5/rest/OrderProxy.java
+++ sep/src/test/java/de/ostfalia/gruppe5/rest/OrderProxy.java
@@ -1,11 +1,10 @@
 package de.ostfalia.gruppe5.rest;
 
 import javax.json.JsonArray;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.json.JsonObject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/orders")
 public interface OrderProxy extends BasicProxy {
@@ -18,4 +17,16 @@ public interface OrderProxy extends BasicProxy {
     @Path("/{id}/orderDetails")
     @Produces(MediaType.APPLICATION_JSON)
     JsonArray getOrderDetails(@PathParam("id") Integer id);
+
+    @POST
+    @Path("/basket")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response postOrderBasket(JsonObject json);
+
+    @POST
+    @Path("/basket/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response postOrderBasket(@PathParam("id") Integer id, JsonObject json);
 }
