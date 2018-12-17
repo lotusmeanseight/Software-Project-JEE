43f4234d0421ec4e41d5f39a2342003f502e8f40
unknown <sakul.bj@googlemail.com>
2018-12-17 21:09:22 +0100

    docker sprint 7

--- sprint-7/start.sh
+++ sprint-7/start.sh
@@ -5,12 +5,14 @@ cd ..
 cd sep
 mvn clean verify
 cd ..
-cd sep-ui
-mvn clean verify
+#cd sep-ui
+#mvn clean verify
+
 # copy the wars in the folders of the dockerfiles.
-cd ..
+# cd ..
 mv sep/target/sep-gruppe-5.war sprint-7/sep
-mv sep-ui/target/sep-ui-gruppe-5.war sprint-7/sep
+#mv sep-ui/target/sep-ui-gruppe-5.war sprint-7/sep
+
 # building and running the docker containers
 cd sprint-7/databases
 docker build -t gruppe5-postgres .
@@ -18,13 +20,16 @@ docker run --name postgres-container -e POSTGRES_PASSWORD=12345 -d gruppe5-postg
 cd ..
 cd sep
 docker build -t gruppe5-sep .
-docker run --name sep-container -d gruppe5-sep
-cd ..
-cd sep-ui
-docker build -t gruppe5-sep-ui .
-docker run --name sep-ui-container -d gruppe5-sep-ui
+docker run --name sep-container -p 8080:8080 -d gruppe5-sep 
+#cd ..
+#cd sep-ui
+#docker build -t gruppe5-sep-ui .
+#docker run --name sep-ui-container -d gruppe5-sep-ui
+
 # defining the docker network
 docker network create gruppe5-network
-docker network connect gruppe5-network gruppe5-postgres
-docker network connect gruppe5-network gruppe5-sep
-docker network connect gruppe5-network gruppe5-sep-ui
+docker network connect gruppe5-network postgres-container
+docker network connect gruppe5-network sep-container
+#docker network connect gruppe5-network gruppe5-sep-ui-container
+
+read name
